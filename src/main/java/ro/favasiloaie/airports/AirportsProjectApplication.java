d8509729262d6a4f6e6e0333bd4995569ea85378
floppi19 <florin.avasiloaie@gmail.com>
2020-11-01 16:56:52 +0200

    create project structure

---
+++ src/main/java/ro/favasiloaie/airports/AirportsProjectApplication.java
@@ -0,0 +1,13 @@
+package ro.favasiloaie.airports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportsProjectApplication.class, args);
+	}
+
+}
